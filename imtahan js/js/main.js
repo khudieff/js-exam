b3e32f4aed37a80f026273ec96923044f7a330ae
Ali Xudiyev <87548328+khudieff@users.noreply.github.com>
2021-08-31 13:55:46 +0400

    Add files via upload

---
+++ imtahan js/js/main.js
@@ -0,0 +1,23 @@
+var ar=["Anar", "Ayxan", "Murad"];
+
+
+console.log(ar);
+var lili=document.querySelector(".box li")
+
+
+
+
+
+
+
+
+var btn=document.querySelectorAll(".tab-menu button")
+var p=document.querySelectorAll(".tab-menu p")
+ 
+btn.onclick=function(){
+for (let i = 0; i < btn.length; i++) {
+     
+
+    
+}
+}
